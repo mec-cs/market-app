913987516addcabaab13afd187305abf819fb52e
Enes <102901204+mec-cs@users.noreply.github.com>
2024-05-11 23:29:39 +0300

    last version db sql tables

--- tables.sql
+++ tables.sql
@@ -24,7 +24,7 @@ SET FOREIGN_KEY_CHECKS=1;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `auth`
+-- Table structure for table `auth_table`
 --
 
 DROP TABLE IF EXISTS `auth_table`;
@@ -39,14 +39,12 @@ CREATE TABLE `auth_table` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `auth`
+-- Dumping data for table `auth_table`
 --
 
-LOCK TABLES `auth_table` WRITE;
-/*!40000 ALTER TABLE `auth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_table` DISABLE KEYS */;
 INSERT INTO `auth_table` VALUES ('ali@gmail.com','$2a$10$dnztcVRKs52MDO/PI/HAkOyjJNVOTKCjaAlE1WOx/u4BH4p452b9W',NULL),('batuuzun81@gmail.com','$2a$10$oek6tXLRJQHOhX8IvF4qcuXsqbCJ4OGBBwce6KtiHwopDllIVOvWK',NULL),('hakansibi@gmail.com','$2a$10$JE7KG/7G8QQ2lYF4UiuyQuNn8MF6rosCYeNVu8zgs/5MYicENu8uW',NULL),('mehmetenes@gmail.com','$2a$10$4QeQjW1m4Zlx0mmKTKunBeNLeLSaywwZIa7Pt3jllTf2u3zgPJyxu',NULL);
-/*!40000 ALTER TABLE `auth` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `auth_table` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -79,7 +77,7 @@ CREATE TABLE `user_table` (
   `email` varchar(100) COLLATE utf8mb4_turkish_ci NOT NULL,
   `name` varchar(100) COLLATE utf8mb4_turkish_ci NOT NULL,
   PRIMARY KEY (`email`),
-  CONSTRAINT `user_auth_fk` FOREIGN KEY (`email`) REFERENCES `auth` (`email`) ON DELETE CASCADE
+  CONSTRAINT `user_auth_table_fk` FOREIGN KEY (`email`) REFERENCES `auth_table` (`email`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -87,11 +85,10 @@ CREATE TABLE `user_table` (
 -- Dumping data for table `user_table`
 --
 
-LOCK TABLES `user_table` WRITE;
+
 /*!40000 ALTER TABLE `user_table` DISABLE KEYS */;
 INSERT INTO `user_table` VALUES ('ali@gmail.com','Ali Gül'),('batuuzun81@gmail.com','Kent Gıda'),('hakansibi@gmail.com','Eti'),('mehmetenes@gmail.com','Ülker');
 /*!40000 ALTER TABLE `user_table` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -115,7 +112,7 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `role`
+-- Table structure for table `role_table`
 --
 
 DROP TABLE IF EXISTS `role_table`;
@@ -123,21 +120,20 @@ DROP TABLE IF EXISTS `role_table`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `role_table` (
   `email` varchar(100) COLLATE utf8mb4_turkish_ci NOT NULL,
-  `role` char(1) COLLATE utf8mb4_turkish_ci NOT NULL,
+  `role_table` char(1) COLLATE utf8mb4_turkish_ci NOT NULL,
   PRIMARY KEY (`email`),
-  CONSTRAINT `role_auth_fk` FOREIGN KEY (`email`) REFERENCES `auth_table` (`email`) ON DELETE CASCADE
+  CONSTRAINT `role_table_auth_table_fk` FOREIGN KEY (`email`) REFERENCES `auth_table` (`email`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `role`
+-- Dumping data for table `role_table`
 --
 
-LOCK TABLES `role_table` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+
+/*!40000 ALTER TABLE `role_table` DISABLE KEYS */;
 INSERT INTO `role_table` VALUES ('ali@gmail.com','C'),('batuuzun81@gmail.com','M'),('hakansibi@gmail.com','M'),('mehmetenes@gmail.com','M');
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `role_table` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -161,7 +157,7 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `address`
+-- Table structure for table `address_table`
 --
 
 DROP TABLE IF EXISTS `address_table`;
@@ -174,20 +170,19 @@ CREATE TABLE `address_table` (
   `district` varchar(25) COLLATE utf8mb4_turkish_ci DEFAULT NULL,
   `addr` varchar(100) COLLATE utf8mb4_turkish_ci NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `address_fk` (`email`),
-  CONSTRAINT `address_fk` FOREIGN KEY (`email`) REFERENCES `user_table` (`email`) ON DELETE CASCADE
+  KEY `address_table_fk` (`email`),
+  CONSTRAINT `address_table_fk` FOREIGN KEY (`email`) REFERENCES `user_table` (`email`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `address`
+-- Dumping data for table `address_table`
 --
 
-LOCK TABLES `address_table` WRITE;
-/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+
+/*!40000 ALTER TABLE `address_table` DISABLE KEYS */;
 INSERT INTO `address_table` VALUES (1,'ali@gmail.com','Kocaeli','Gebze','Asım Sokak no:70/54'),(5,'batuuzun81@gmail.com','Istanbul','Kadıköy','Göztepe Mahallesi, Afacan Sokak No:61/55'),(6,'mehmetenes@gmail.com','Ankara','Bahçelievler','Sorar Sokak no:14/7'),(7,'hakansibi@gmail.com','Ankara','Kızılay','Vadi Sokak no:78/3');
-/*!40000 ALTER TABLE `address` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `address_table` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -210,7 +205,7 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `company`
+-- Table structure for table `company_table`
 --
 
 DROP TABLE IF EXISTS `company_table`;
@@ -219,24 +214,23 @@ DROP TABLE IF EXISTS `company_table`;
 CREATE TABLE `company_table` (
   `c_id` int NOT NULL AUTO_INCREMENT,
   `c_name` varchar(255) COLLATE utf8mb4_turkish_ci DEFAULT NULL,
-  `c_address` int DEFAULT NULL,
-  `number_of_products` int DEFAULT NULL,
+  `c_address_table` int DEFAULT NULL,
+  `number_of_product_tables` int DEFAULT NULL,
   `c_image` varchar(100) COLLATE utf8mb4_turkish_ci DEFAULT NULL,
   PRIMARY KEY (`c_id`),
-  KEY `c_address` (`c_address`),
-  CONSTRAINT `company_ibfk_1` FOREIGN KEY (`c_address`) REFERENCES `address_table` (`id`)
+  KEY `c_address_table` (`c_address_table`),
+  CONSTRAINT `company_table_ibfk_1` FOREIGN KEY (`c_address_table`) REFERENCES `address_table` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `company`
+-- Dumping data for table `company_table`
 --
 
-LOCK TABLES `company_table` WRITE;
-/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+
+/*!40000 ALTER TABLE `company_table` DISABLE KEYS */;
 INSERT INTO `company_table` VALUES (1,'Kent Gıda',5,1,'kentgida.png'),(2,'Ülker',6,1,'ulker_logo_png.png'),(3,'Eti',7,2,'eti.jpg');
-/*!40000 ALTER TABLE `company` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `company_table` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -259,7 +253,7 @@ UNLOCK TABLES;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `product`
+-- Table structure for table `product_table`
 --
 
 DROP TABLE IF EXISTS `product_table`;
@@ -275,19 +269,18 @@ CREATE TABLE `product_table` (
   `p_price` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`p_id`),
   KEY `c_id` (`c_id`),
-  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `company_table` (`c_id`)
+  CONSTRAINT `product_table_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `company_table` (`c_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `product`
+-- Dumping data for table `product_table`
 --
 
-LOCK TABLES `product_table` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+
+/*!40000 ALTER TABLE `product_table` DISABLE KEYS */;
 INSERT INTO `product_table` VALUES (1,'Falım Naneli',50,'2024-08-15',1,'falım_nane.jpg',1.50),(2,'Ülker Çikolatalı Gofret',100,'2024-06-30',2,'ülker_gofret.jpg',5.99),(3,'Eti Sütlü Çikolata',50,'2024-08-15',3,'eti_sütlü_çikolata.jpg',8.50),(4,'Eti Cin',75,'2024-07-20',3,'eti_cin.jpg',3.75);
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
+/*!40000 ALTER TABLE `product_table` ENABLE KEYS */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 COMMIT;
